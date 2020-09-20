f862336509cdbb77f01e7a8072fb70c3632769a0
irinazoccolini <71565864+irinazoccolini@users.noreply.github.com>
2020-09-20 01:44:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var questionOneArticles = ["", "test", "one"]
+
+
+function getResults(){
+  document.getElementById("suggestiontitle").innerHTML = "Your Recommendations"
+  document.getElementById("suggestion1").innerHTML = "A good way to keep busy while at home is to try a new hobby. Check out this website with ideas: <a href='https://parade.com/1038103/jessicasager/hobbies-to-pick-up-during-quarantine/'> https://parade.com/1038103/jessicasager/hobbies-to-pick-up-during-quarantine/<a>"
+  document.getElementById("suggestion2").innerHTML ="If you're looking for ways to destress, you can try one of the suggestions on this website: <a href='https://www.virtua.org/articles/10-quick-ways-to-de-stress'>https://www.virtua.org/articles/10-quick-ways-to-de-stress</a>"
+  document.getElementById("suggestion3").innerHTML = "When you want to spend time socializing with friends, you can try one of these suggestions: <a href''https://medium.com/youalberta/how-to-stay-connected-with-friends-during-social-distancing-fbaf1d44fb24'> https://medium.com/youalberta/how-to-stay-connected-with-friends-during-social-distancing-fbaf1d44fb24</a>"
+    
+  
+}
+
+
 
